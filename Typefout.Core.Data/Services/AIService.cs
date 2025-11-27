062c3bfa92ead7298c77d9bd0ac8bfd014a1414a
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2025-11-27 14:23:19 +0100

    setup model + service

---
+++ Typefout.Core.Data/Services/AIService.cs
@@ -0,0 +1,13 @@
+﻿using Typefout.Core.Interfaces;
+using Typefout.Core.Models;
+
+namespace Typefout.Core.Data.Services
+{
+    public class AIService : IAIService
+    {
+        public TypingExerciseText GetExerciseText()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
