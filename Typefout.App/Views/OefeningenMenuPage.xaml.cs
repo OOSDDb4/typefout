c4738c7f498cab40544b64e4d22c1564e0b7157f
jeroenWindesheim <S1215501@student.windesheim.nl>
2025-12-08 14:27:43 +0100

    Functionality when database testing button is clicked

--- Typefout.App/Views/OefeningenMenuPage.xaml.cs
+++ Typefout.App/Views/OefeningenMenuPage.xaml.cs
@@ -1,6 +1,11 @@
+using System.Data;
+using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Typefout.App.ViewModels;
 using Typefout.App.Views;
+using Typefout.Core;
+using Typefout.Core.Data.Services;
+using Typefout.Core.Interfaces;
 
 namespace Typefout.App.Views;
 
@@ -22,4 +27,25 @@ public partial class OefeningenMenuPage : ContentPage
         SentenceViewModel vm = App.Services.GetRequiredService<SentenceViewModel>();
         await Navigation.PushAsync(new SentenceView(vm));
     }
+
+    private async void Database_Clicked(object sender, EventArgs e)
+    {
+        Trace.WriteLine("DatabaseClicked");
+
+        IDatabaseService service = new DatabaseService();
+        service.Connect();
+        service.Open();
+        DataTable words = service.ExecuteQuery("SELECT * FROM Word");
+
+        foreach (DataRow row in words.Rows)
+        {
+            Trace.WriteLine($"{row["wordId"]}, {row["word"]}");
+        }
+
+        service.Close();
+
+        // DatabaseViewModel dvm = App.Services.GetRequiredService<DatabaseViewModel>();
+        // await Navigation.PushAsync(new DatabaseView(dvm));
+    }
+    
 }
