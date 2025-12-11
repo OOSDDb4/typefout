a8b7e4fd8d85c6084ed343fa9684a737f30b8537
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2025-12-11 10:47:22 +0100

    Mainpage + little fix inside the resultspage

---
+++ Typefout.App/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Typefout.App.ViewModels;
+using Typefout.App.Views;
+
+namespace Typefout.App.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void InlogButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new OefeningenMenuPage());
+    }
+}
