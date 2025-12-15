30b36b4c41604e33d34638315f596258859881b7
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2025-12-15 10:14:53 +0100

    AuthService

---
+++ Typefout.Core/Services/AuthService.cs
@@ -0,0 +1,34 @@
+﻿using Typefout.Core.Helper;
+using Typefout.Core.Interfaces;
+using Typefout.Core.Models;
+
+namespace Typefout.Core.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IUserRepo _userRepo;
+        public AuthService(IUserRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+        public User? Login(string? username, string? email, string password)
+        {
+            if (username is not null)
+            {   
+                User? user = _userRepo.GetUser(username);
+                if (user is null) return null;
+                if (PasswordHelper.VerifyPassword(password, user.Password)) return user;
+                return null;
+            }
+            else if (email is not null)
+            {
+                User? user = _userRepo.GetMail(email);
+                if (user is null) return null;
+                if (PasswordHelper.VerifyPassword(password, user.Password)) return user;
+                return null;
+            }
+            else
+                return null;
+        }
+    }
+}
