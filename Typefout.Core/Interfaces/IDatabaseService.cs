bf72bbea35f49f14ea28e919b307388022fef71e
jeroenWindesheim <S1215501@student.windesheim.nl>
2025-12-22 14:48:54 +0100

    Added the functionality of Create and Read

--- Typefout.Core/Interfaces/IDatabaseService.cs
+++ Typefout.Core/Interfaces/IDatabaseService.cs
@@ -7,9 +7,9 @@ public interface IDatabaseService
     public int Connect();
     public void Open();
     public void Close();
-    public DataTable ExecuteQuery(string sql);
-    public void Create();
-    public void Read();
+    public DataTable ReadQuery(string sql);
+    public int Create(string table, Dictionary<string, object> data);
+    public DataTable Read(string table, List<string>? columns = null, string? where = null, string? joins = null, string? orderBy = null, int? limit = null);
     public void Update();
     public void Delete();
 }
