08469867b8388dceb3ae67fb20634220a98641fc
OzoOnne <55187541+OzoOnne@users.noreply.github.com>
2026-01-05 13:57:30 +0100

    password reset page 3 done

--- Typefout.App/Views/PasswordReset3Page.xaml.cs
+++ Typefout.App/Views/PasswordReset3Page.xaml.cs
@@ -6,4 +6,28 @@ public partial class PasswordReset3Page : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnDoorClicked(object sender, EventArgs e)
+    {
+        string pass1 = NewPasswordEntry.Text;
+        string pass2 = RepeatPasswordEntry.Text;
+
+        if (string.IsNullOrEmpty(pass1) || string.IsNullOrEmpty(pass2))
+        {
+            await DisplayAlert("Fout", "Vul beide wachtwoordvelden in.", "OK");
+            return; 
+        }
+
+        if (pass1 != pass2)
+        {
+            await DisplayAlert("Fout", "De wachtwoorden komen niet overeen. Probeer het opnieuw.", "OK");
+
+            RepeatPasswordEntry.Text = string.Empty;
+            return;
+        }
+
+        await DisplayAlert("Succes", "Je wachtwoord is gewijzigd!", "Klaar");
+
+        Application.Current.MainPage.Navigation.PushAsync(new MainPage());
+    }
 }
