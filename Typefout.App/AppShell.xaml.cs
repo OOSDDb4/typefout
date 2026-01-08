8aea3744431d2ba2864445294b6a7b34557f143b
esnerv <s1182106@student.windesheim.nl>
2026-01-08 19:42:30 +0100

    Kleine fouten opgelost
    Visuele verbetering op het registratiescherm + andere naam voor het knop

--- Typefout.App/AppShell.xaml.cs
+++ Typefout.App/AppShell.xaml.cs
@@ -14,6 +14,6 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
         Routing.RegisterRoute(nameof(OefeningenMenuPage), typeof(OefeningenMenuPage));
         Routing.RegisterRoute(nameof(SchoolsPage), typeof(SchoolsPage));
-        Routing.RegisterRoute("RegistrationPage", typeof(RegistrationPage));
+        Routing.RegisterRoute(nameof(RegistrationPage), typeof(RegistrationPage));
     }
 }
